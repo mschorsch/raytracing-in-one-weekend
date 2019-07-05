a8d75712067f13d69ca8c188080527cad763d809
Matthias Schorsch <matthias.schorsch@gmx.de>
2019-07-05 13:05:46 +0200

    chapter 4

---
+++ src/hitable.rs
@@ -0,0 +1,131 @@
+use crate::ray::Ray;
+use crate::vec3::Vec3;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Hit {
+    pub t: f32,
+    pub p: Vec3,
+    pub normal: Vec3,
+}
+
+impl Hit {
+    pub fn new(t: f32, p: Vec3, normal: Vec3) -> Self {
+        Hit { t, p, normal }
+    }
+}
+
+pub trait Hitable {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<Hit>;
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Sphere {
+    pub center: Vec3,
+    pub radius: f32,
+}
+
+impl Sphere {
+    pub fn new(center: Vec3, radius: f32) -> Self {
+        Sphere { center, radius }
+    }
+}
+
+impl Hitable for Sphere {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<Hit> {
+        // ax^2 + bx + c = 0
+        let a = r.direction.dot(&r.direction);
+        let b = 2.0 * r.direction.dot(&(r.origin - self.center));
+        let c =
+            (r.origin - self.center).dot(&(r.origin - self.center)) - (self.radius * self.radius);
+        let discriminant: f32 = b * b - 4.0 * a * c;
+        if discriminant > 0.0 {
+            let temp = (-b - (b * b - 4.0 * a * c).sqrt()) / (2.0 * a); // midnight formula => wrong in pdf
+            if temp < t_max && temp > t_min {
+                let t = temp;
+                let p = r.point_at_parameter(t);
+                let normal = (p - self.center) / self.radius; // normal unit vector?
+                return Some(Hit::new(t, p, normal));
+            }
+
+            let temp = (-b + (b * b - 4.0 * a * c).sqrt()) / (2.0 * a); // midnight formula => wrong in pdf
+            if temp < t_max && temp > t_min {
+                let t = temp;
+                let p = r.point_at_parameter(t);
+                let normal = (p - self.center) / self.radius; // normal unit vector?
+                return Some(Hit::new(t, p, normal));
+            }
+        }
+        None
+    }
+}
+
+pub struct World {
+    pub hitables: Vec<Box<Hitable>>,
+}
+
+impl World {
+    pub fn new(hitables: Vec<Box<Hitable>>) -> Self {
+        World { hitables }
+    }
+}
+
+impl Hitable for World {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<Hit> {
+        let mut hit_anything = None;
+        let mut closest_so_far = t_max;
+        for hitable in &self.hitables {
+            if let Some(hit) = hitable.hit(r, t_min, closest_so_far) {
+                hit_anything = Some(hit);
+                closest_so_far = hit.t;
+            }
+        }
+        hit_anything
+    }
+}
+
+// impl<T: Hitable + 'static> Hitable for Vec<T> {
+//     fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<Hit> {
+//         self::<Vec<&Hitable>>.hit(r, t_min, t_max)
+//     }
+// }
+
+// impl<'a> Hitable for Vec<&'a Hitable> {
+//     fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<Hit> {
+//         let mut hit_anything = None;
+//         let mut closest_so_far = t_max;
+//         for hitable in self {
+//             if let Some(hit) = hitable.hit(r, t_min, closest_so_far) {
+//                 closest_so_far = hit.t;
+//                 hit_anything = Some(hit);
+//             }
+//         }
+//         hit_anything
+//     }
+// }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_world() {
+        let s1 = Sphere::new(Vec3(0.0, 0.0, 0.0), 1.0);
+        let s2 = Sphere::new(Vec3(0.0, 0.0, 0.0), 2.0);
+        let hitables: Vec<Box<Hitable>> = vec![Box::new(s1), Box::new(s2)];
+        let world = World::new(hitables);
+
+        let r = Ray::new(Vec3(0.0, 0.0, 0.0), Vec3(0.0, 0.0, -1.0));
+        assert_eq!(world.hit(&r, 0.0, 1.0), None);
+    }
+
+    // #[test]
+    // fn test_hitable_vec() {
+    //     let s1 = Sphere::new(Vec3(0.0, 0.0, 0.0), 1.0);
+    //     let s2 = Sphere::new(Vec3(0.0, 0.0, 0.0), 2.0);
+    //     let v: Vec<Sphere> = vec![s1, s2];
+
+    //     let r = Ray::new(Vec3(0.0, 0.0, 0.0), Vec3(0.0, 0.0, -1.0));
+    //     assert_eq!(v.hit(&r, 0.0, 1.0), None);
+    // }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
+mod hitable;
 mod ray;
 mod vec3;
 use std::fs;
 use std::io;
 use std::io::prelude::*;
 
+use hitable::{Hit, Hitable, Sphere, World};
 use ray::Ray;
 use vec3::{unit_vector, Vec3};
 
@@ -25,12 +27,19 @@ fn main() -> io::Result<()> {
     let vertical = Vec3(0.0, 2.0, 0.0);
     let origin = Vec3(0.0, 0.0, 0.0);
 
+    let world = World::new(vec![
+        Box::new(Sphere::new(Vec3(0.0, 0.0, -1.0), 0.5)),
+        Box::new(Sphere::new(Vec3(0.0, -100.5, -1.0), 100.0)),
+    ]);
+
     for j in (0..ROWS).rev() {
         for i in 0..COLUMNS {
             let u = i as f32 / COLUMNS as f32;
             let v = j as f32 / ROWS as f32;
             let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
-            let col = color(&r);
+
+            // let p = r.point_at_parameter(2.0); // 2.0??
+            let col = color(&r, &world);
 
             let ir = (255.99 * col.0) as u32;
             let ig = (255.99 * col.1) as u32;
@@ -43,10 +52,22 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn color(r: &Ray) -> Vec3 {
-    // linear blend; lineai interpolation; lerp
-    // blended_value = (1-t)*start_value + t*end_value
-    let unit_direction = unit_vector(r.direction); // unit vector -1 <= x <= 1
-    let t = 0.5 * (unit_direction.y() + 1.0); // scale to 0 <= x <= 1
-    (1.0 - t) * Vec3(1.0, 1.0, 1.0) /* white */ + t * Vec3(0.5, 0.7, 1.0) /* blue */ // blend blue to white
+fn color(r: &Ray, world: &World) -> Vec3 {
+    use std::f32;
+
+    if let Some(hit) = world.hit(r, 0.0, f32::MAX) {
+        // scale to 0 <= x <= 1
+        0.5 * Vec3(
+            hit.normal.x() + 1.0,
+            hit.normal.y() + 1.0,
+            hit.normal.z() + 1.0,
+        )
+    } else {
+        // linear blend; linear interpolation; lerp
+        // blended_value = (1-t)*start_value + t*end_value
+        let unit_direction = unit_vector(r.direction); // unit vector -1 <= x <= 1
+        let t = 0.5 * (unit_direction.y() + 1.0); // scale to 0 <= x <= 1
+                                                  // blend blue to white
+        (1.0 - t) * Vec3(1.0, 1.0, 1.0) /* white */ + t * Vec3(0.5, 0.7, 1.0) /* blue */
+    }
 }
